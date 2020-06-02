9b56c5b3e1e97b7a4ebbdd3fd8a381c519bc9e7d
Yogesh Singh <yogesh@clevertap.com>
2020-06-02 22:23:58 +0530

    SDK-167: setup testing for mParticle CleverTap integration

---
+++ mParticle_CleverTapTests/MPKitCleverTapSpec.m
@@ -0,0 +1,19 @@
+
+@import Quick;
+@import Nimble;
+@import OCMock;
+@import CleverTapSDK;
+@import mParticle_Apple_SDK;
+#import "MPKitCleverTap.h"
+
+
+QuickSpecBegin(MPKitCleverTapSpec)
+
+describe(@"a mParticle CleverTap integration", ^{
+    
+    it(@"", ^{
+        
+    });
+});
+
+QuickSpecEnd
