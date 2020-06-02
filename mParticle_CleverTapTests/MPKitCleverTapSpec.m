f8dba2e1e5a08d1c116ddb2d25d4c53c9279cf63
Yogesh Singh <yogesh@clevertap.com>
2020-06-02 22:35:50 +0530

    SDK-167: added tests to verify integration instance starts on launch

--- mParticle_CleverTapTests/MPKitCleverTapSpec.m
+++ mParticle_CleverTapTests/MPKitCleverTapSpec.m
@@ -11,8 +11,13 @@ QuickSpecBegin(MPKitCleverTapSpec)
 
 describe(@"a mParticle CleverTap integration", ^{
     
-    it(@"", ^{
-        
+    context(@"conforming to MPKitProtocol", ^{
+       
+        it(@"is started on launching with Configuration", ^{
+            MPKitCleverTap *clevertapKit = [MPKitCleverTap new];
+            [clevertapKit didFinishLaunchingWithConfiguration:@{@"AccountID":@"12345", @"AccountToken":@"54321"}];
+            expect(clevertapKit.started).to(beTrue());
+        });
     });
 });
 
